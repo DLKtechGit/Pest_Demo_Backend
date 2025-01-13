0175d8cb0b6dc4f54291a5cb2a62257ca73d80fd
DLKReact@2024 <dlktechnologiesreact@gmail.com>
2025-01-13 12:09:49 +0530

    Initial commit

---
+++ env/env.js
@@ -0,0 +1,16 @@
+const env = {   
+    SECRET_TOKEN: "Pest123",
+    // DB_CONNECTION_URL:"mongodb://pestpatrol:Pest12345@ac-dqbhjfs-shard-00-00.9zo8ck5.mongodb.net:27017,ac-dqbhjfs-shard-00-01.9zo8ck5.mongodb.net:27017,ac-dqbhjfs-shard-00-02.9zo8ck5.mongodb.net:27017/?ssl=true&replicaSet=atlas-7n9anp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+    // DB_CONNECTION_URL:'mongodb+srv://dlk:dlkTechnologies@cluster0.9zo8ck5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        
+    // DB_CONNECTION_URL: 'mongodb+srv://dlk:dlkTechnologies@cluster0.9zo8ck5.mongodb.net/Pest_clone?retryWrites=true&w=majority&appName=Cluster0'
+    // DB_CONNECTION_URL:'mongodb+srv://user1:vijayaraj@cluster0.7nua9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    DB_CONNECTION_URL:'mongodb+srv://user1:vijay@cluster0.7nua9.mongodb.net/test'
+} 
+module.exports = env;    
+  
+
+    
+// mongodb+srv://dlk:dlkTechnologies@cluster0.9zo8ck5.mongodb.net/dlk_clone?retryWrites=true&w=majority&appName=Cluster0'
+
+    
